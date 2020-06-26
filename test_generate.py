8de772969919f60206e1c68a34d432f38e5e0fe2
Muhammad Musa <moosasaadat99@gmail.com>
2020-06-26 22:59:26 +0500

    Add test case for enforce_node_consistency()

--- test_generate.py
+++ test_generate.py
@@ -1,8 +1,21 @@
 import unittest
-import generate
+from crossword import *
+from generate import *
+
 
 class TestGenerate(unittest.TestCase):
-    pass
+    def test_enforce_node_consistency(self):
+        # Parse command-line arguments
+        structure = "data/structure1.txt"
+        words = "data/words1.txt"
+
+        # Generate crossword
+        crossword = Crossword(structure, words)
+        creator = CrosswordCreator(crossword)
+        creator.enforce_node_consistency()
+        for variable, words in creator.domains.items():
+            print(f"{variable}: {words}")
+
 
 if __name__ == "__main__":
     unittest.main()
